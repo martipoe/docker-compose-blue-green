5d2b67a7e35f8f85ff0e40c90071ad026dbcc6f2
martipoe <130643472+martipoe@users.noreply.github.com>
2024-12-31 03:48:12 +0100

    feat: initial implementation

---
+++ README.md
@@ -0,0 +1,77 @@
+# docker-compose-blue-green
+
+Example implementation for Blue-Green deployments with Docker Compose and Traefik.
+
+While testing similar implementations at https://github.com/straypaper/blue-green and https://frustrated.blog/2021/03/16/traefik_blue_green.html, I encountered problems with [Traefik File Provider](https://doc.traefik.io/traefik/providers/file/) and docker bind mounts - even though *directory* configuration and *providersThrottleDuration* were set, there still were 404s for up to 60s during dynamic configuration changes.
+
+Workaround:
+- These issues did not arise with the [HTTP Provider](https://doc.traefik.io/traefik/providers/http/). This is why an Nginx container serves the dynamic configuration as yaml template via HTTP, using https://nginx.org/en/docs/http/ngx_http_sub_module.html for dynamic contents.
+- During Nginx container restarts (updates...), Traefik will keep the dynamic configuration in memory until the HTTP endpoint is reachable again.
+
+## Prerequisites
+
+- Add domains in */etc/hosts*: `127.0.0.1 localhost main.lan blue.lan green.lan`
+- Create external docker network for backend communication: `docker network create traefik_proxy`
+- Generate self-signed SSL certificate `openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./traefik/certs/cert.key -out ./traefik/certs/cert.crt`
+- For Green and Blue service, the **container_name** in docker-compose.yml must be explicitely set to either *green* or *blue* (see *project.docker-compose.yml*).
+- The docker host needs yq `apt install -y yq`
+
+## Usage
+
+Bring up Traefik and Backend:
+```bash
+docker compose -f traefik.docker-compose.yml up -d
+docker compose -f project.docker-compose.yml up -d
+```
+
+*.env*:
+```bash
+# old images
+DOCKER_IMAGE_BLUE=php:8.2.27-apache@sha256:56d8b242c3430aa5eb27fc112194d2b22c5c72fe7c3b3db0940639f735154c55
+DOCKER_IMAGE_GREEN=php:8.2.27-apache@sha256:56d8b242c3430aa5eb27fc112194d2b22c5c72fe7c3b3db0940639f735154c55
+```
+
+Run continuous HTTP check:
+```bash
+while true; do
+    curl -k https://main.lan
+    sleep 1
+done
+```
+
+Deploy new container version #1: `bash deploy.sh "php:8.3-apache@sha256:fce243539486d99cfefba35724ec485fd6078f1d4928feba5728d3ca587f8820"`
+```bash
+# blue is updated in .env
+DOCKER_IMAGE_BLUE=php:8.3-apache@sha256:fce243539486d99cfefba35724ec485fd6078f1d4928feba5728d3ca587f8820
+DOCKER_IMAGE_GREEN=php:8.2.27-apache@sha256:56d8b242c3430aa5eb27fc112194d2b22c5c72fe7c3b3db0940639f735154c55
+
+:~$ docker ps
+CONTAINER ID   IMAGE                     COMMAND                  CREATED          STATUS          PORTS                                      NAMES
+c514e4d21936   php:8.3-apache            "docker-php-entrypoi…"   44 seconds ago   Up 42 seconds   80/tcp                                     blue
+```
+
+Deploy new container version #2: `bash deploy.sh "php:8.4.2-apache@sha256:2c9ae64a55950a3b44c5121cae9b1dc82601e9ff2a0ed0279d02c047019ca53d"`
+```bash
+# green is updated in .env
+DOCKER_IMAGE_BLUE=php:8.3-apache@sha256:fce243539486d99cfefba35724ec485fd6078f1d4928feba5728d3ca587f8820
+DOCKER_IMAGE_GREEN=php:8.4.2-apache@sha256:2c9ae64a55950a3b44c5121cae9b1dc82601e9ff2a0ed0279d02c047019ca53d
+
+:~$ docker ps
+CONTAINER ID   IMAGE                     COMMAND                  CREATED              STATUS              PORTS                                      NAMES
+7f2a89711237   php:8.4.2-apache          "docker-php-entrypoi…"   18 seconds ago       Up 16 seconds       80/tcp                                     green
+```
+
+## Caveats
+
+- Only tested on Debian 12.
+- The deployment script only accepts images with sha256 hashes for security reasons, read https://candrews.integralblue.com/2023/09/always-use-docker-image-digests/.
+
+## Todos
+
+- deploy.sh:
+    - Add automatic rollbacks - currently the script only leaves the lock enabled to avoid re-runs until it is removed manually.
+    - Handling of additional commented tasks (database backups and migrations,...)
+    - Use `shellcheck deploy.sh -x` for new commits
+- Traefik:
+    - Let's Encrypt
+    - ACLs
---
+++ deploy.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+DOCKER_IMAGE_NEW="$1"
+
+DOCKER_COMPOSE_CONFIG="project.docker-compose.yml"
+# set to true if container provides integrated healthcheck
+DOCKER_HEALTHCHECK=false
+
+# Create lockfile to avoid race conditions due to multiple deploys
+LOCKFILE=./deploy.lock
+if [ -f "${LOCKFILE}" ]; then
+    echo "Failure: Lock file exists, exiting."
+    exit 1
+fi
+touch "${LOCKFILE}"
+
+
+# Validation checks
+if ! [ -f ".env" ]; then
+    echo "Failire: .env is missing."
+    rm "${LOCKFILE}"
+    exit 1
+else
+    # requires URL_MAIN, URL_BLUE, URL_GREEN
+    source .env
+fi
+
+if ! [[ $(dpkg -l yq) ]]; then
+    echo "Failure: Dependency yq is not installed (apt install yq)."
+    rm "${LOCKFILE}"
+    exit 1
+fi
+
+echo "Validating image name..."
+if ! [[ "${DOCKER_IMAGE_NEW}" =~ ^.*:.*@sha256:[a-z0-9]{64}$ ]]; then
+    echo "Failure: Image name ${DOCKER_IMAGE_NEW} did not pass validation checks. Ensure the sha256-hash is also provided."
+    rm "${LOCKFILE}"
+    exit 1
+fi
+
+
+# Pull docker image
+echo "Pulling docker image ${DOCKER_IMAGE_NEW}..."
+if ! [[ $(docker pull "${DOCKER_IMAGE_NEW}") ]]; then
+    echo "Failure: ${DOCKER_IMAGE_NEW} does not exist."
+    rm "${LOCKFILE}"
+    exit 1
+fi
+
+# Detect running container from Traefik dynamic configuration
+echo "Detecting active container and image version..."
+SERVICE_ACTIVE=$(yq -r .http.routers.main.service nginx/dynamic/http.routers.main.yml)
+CONTAINER_OLD="${SERVICE_ACTIVE%%@*}"
+if ! [[ "${CONTAINER_OLD}" =~ ^(blue|green)$ ]]; then
+    echo "Failure: No container is active."
+    rm "${LOCKFILE}"
+    exit 1
+else
+    DOCKER_IMAGE_OLD=$(docker inspect --format '{{.Config.Image}}' "${CONTAINER_OLD}")
+    echo "${CONTAINER_OLD} currently runs image ${DOCKER_IMAGE_OLD}"
+fi
+
+# Update image version in .env
+echo "Updating docker image version for ${CONTAINER_OLD} to ${DOCKER_IMAGE_NEW} in .env"
+if [[ "${CONTAINER_OLD}" == "blue" ]]; then
+    sed -i "s|^DOCKER_IMAGE_GREEN=.*$|DOCKER_IMAGE_GREEN=${DOCKER_IMAGE_NEW}|g" .env
+    CONTAINER_NEW="green"
+elif [[ "${CONTAINER_OLD}" == "green" ]]; then
+    sed -i "s|^DOCKER_IMAGE_BLUE=.*$|DOCKER_IMAGE_BLUE=${DOCKER_IMAGE_NEW}|g" .env
+    CONTAINER_NEW="blue"
+fi
+
+# Todo: Implement backups for rollbacks here
+# Todo: Implement database locking here if required
+
+# Launch new container
+echo "Starting ${CONTAINER_NEW} with docker image ${DOCKER_IMAGE_NEW} and wait 5s for container to start..."
+docker compose -f "${DOCKER_COMPOSE_CONFIG}" up -d "${CONTAINER_NEW}"
+sleep 5
+
+echo "Ensure container ${CONTAINER_NEW} uses the new image ${DOCKER_IMAGE_NEW}..."
+if ! [[ "$(docker inspect --format '{{.Config.Image}}' "${CONTAINER_NEW}")" == "${DOCKER_IMAGE_NEW}" ]]; then
+    echo "Failure: Container ${CONTAINER_NEW} still uses the old image."
+    rm "${LOCKFILE}"
+    exit 1
+fi
+
+# Optional: Docker Healthcheck
+if [[ "${DOCKER_HEALTHCHECK}" == "true" ]]; then
+    echo "Checking Docker Healthcheck status..."
+    if [[ "$(docker container inspect --format='{{.State.Health.Status}}' "${CONTAINER_NEW}")" == 'healthy' ]]; then
+        echo "Container ${CONTAINER_NEW} is healthy"
+    else
+        echo "Failure: Healthcheck for container ${CONTAINER_NEW} failed, but container ${CONTAINER_OLD} is still active."
+        # Todo: Implement rollback here
+        exit 1
+    fi
+fi
+
+# Todo: Implement migrations for new image here
+
+# HTTP health check on dedicated container url
+echo "Ensure the container ${CONTAINER_NEW} can be accessed with HTTP status code 200"
+if [[ "${CONTAINER_NEW}" == "blue" ]]; then
+    HTTP_STATUS=$(curl -k --silent --output /dev/null --write-out "%{http_code}" "https://${URL_BLUE}")
+    if [[ "${HTTP_STATUS}" != '200' ]]; then
+        echo "Failure: HTTP check for https://${URL_BLUE} failed with code ${HTTP_STATUS}"
+        # Todo: Implement rollback here
+        exit 1
+    fi
+elif [[ "${CONTAINER_NEW}" == "green" ]]; then
+    HTTP_STATUS=$(curl -k --silent --output /dev/null --write-out "%{http_code}" "https://${URL_GREEN}")
+    if [[ "${HTTP_STATUS}" != '200' ]]; then
+        echo "Failure: HTTP check for https://${URL_GREEN} failed with code ${HTTP_STATUS}"
+        # Todo: Implement rollback here
+        exit 1
+    fi
+fi
+
+# Promote new container to serve requests on primary url
+echo "Switching traffic to ${CONTAINER_NEW}..."
+yq -yi --arg c "${CONTAINER_NEW}" '.http.routers.main.service = $c + "@docker"' nginx/dynamic/http.routers.main.yml
+sleep 3
+
+# HTTP health check on primary url
+echo "Ensure the container ${CONTAINER_NEW} can be accessed at http://${URL_MAIN} with HTTP status code 200"
+HTTP_STATUS=$(curl -k --silent --output /dev/null --write-out "%{http_code}" "https://${URL_MAIN}")
+if [[ "${HTTP_STATUS}" != '200' ]]; then
+    echo "Failure: HTTP check for https://${URL_MAIN} failed with code ${HTTP_STATUS}"
+    # Todo: Implement rollback here
+    exit 1
+fi
+
+# Todo: Implement database unlocking here if required
+
+# Cleanup
+echo "Decomissioning outdated container ${CONTAINER_OLD}..."
+docker compose -f "${DOCKER_COMPOSE_CONFIG}" down "${CONTAINER_OLD}"
+
+
+# Remove lockfile
+rm "${LOCKFILE}"
